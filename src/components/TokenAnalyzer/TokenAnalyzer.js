bfaf6ba6b1a4e7033ec0ddf8d5ae99998ac00c2b
codingwithahmed <aziahmedfarooq@gmail.com>
2022-06-09 13:55:54 -0700

    wallet analyzer complete | working on token analyzer

--- src/components/TokenAnalyzer/TokenAnalyzer.js
+++ src/components/TokenAnalyzer/TokenAnalyzer.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import ContentContainer from '../ContentContainer/ContentContainer'
+import Select from 'react-select'
 
 const TokenAnalyzer = () => {
+
+  
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+
   return (
-    <div>TokenAnalyzer</div>
+    <div className='w-full h-[100vh] flex flex-col justify-center items-center'>
+    <ContentContainer>
+      <h1 className='font-bold text-xl text-center w-[95%]'>
+        Select a token to analyze
+      </h1>
+      <Select className='rounded-lg w-[225px] lg:w-[400px] mt-4  border-2' options={options} />
+      <button className='mt-6 bg-slate-900 hover:bg-slate-700 rounded-lg text-white px-6 py-2 w-[200px]'>Analyze Token</button> 
+    </ContentContainer>
+</div>
   )
 }
 
--- src/components/WalletAnalyzer/WalletAnalyzer.js
+++ src/components/WalletAnalyzer/WalletAnalyzer.js
@@ -5,11 +5,11 @@ const WalletAnalyzer = () => {
   return (
     <div className='w-full h-[100vh] flex flex-col justify-center items-center'>
         <ContentContainer>
-          <h1 className='font-bold text-xl'>
+          <h1 className='font-bold text-xl text-center w-[95%]'>
             Enter a wallet address to analyze
           </h1>
-          <input className='bg-slate-50 rounded-lg w-[400px] px-4 py-2 mt-4 border-slate-100 border-2' type={'text'} placeholder={'Wallet address'} />
-          <button className=''>Analyze Wallet</button>
+          <input className='bg-slate-50 rounded-lg w-[225px] lg:w-[400px] px-4 py-2 mt-4 border-slate-100 border-2' type={'text'} placeholder={'Wallet address'} />
+          <button className='mt-6 bg-slate-900 hover:bg-slate-700 rounded-lg text-white px-6 py-2 w-[200px]'>Analyze Wallet</button> 
         </ContentContainer>
     </div>
   )
